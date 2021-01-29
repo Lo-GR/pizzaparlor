dc9693ebcaad1a989bb1181ef910ad85f0a21dbf
Lo-GR <diamondintheroth@gmail.com>
2021-01-29 10:04:20 -0800

    write specs for findPrice prototype function

--- js/specs.js
+++ js/specs.js
@@ -1,3 +1,10 @@
 
 Describe: Pizza.prototype.findPrice
-Test: "it returns "
+Test: "it returns a price based on the property of "this.price" from a Pizza object"
+Expect(Pizza.prototype.findPrice().toEqual(10))
+Test: "it returns a price adjusted by having bacon be a property of a pizza object"
+Expect(Pizza.prototype.findPrice().toEqual(11))
+Test: "it returns a price adjusted by having Pineapple be a property of a pizza object"
+Expect(Pizza.prototype.findPrice().toEqual(12))
+Test: "it returns a price adjusted by the size property of a pizza objet, 'expect' based on selecting small"
+Expect(Pizza.prototype.findPrice().toEqual(8))
