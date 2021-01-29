fe95bab414d243cd5be966858e67a39191aac8c4
Lo-GR <diamondintheroth@gmail.com>
2021-01-29 10:23:41 -0800

    amend specs slightly, pass 1/4 tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
+function Pizza(){
+  this.meat
+  this.cheese 
+  this.sauce 
+  this.extra 
+  this.size
+  this.price = 10;
+}
 
+pizza = new Pizza;
+
+Pizza.prototype.findPrice = function(pizzaObject){
+  return this.price;
+}
 
 $(document).ready(function() {
 
--- js/specs.js
+++ js/specs.js
@@ -1,10 +1,25 @@
 
 Describe: Pizza.prototype.findPrice
 Test: "it returns a price based on the property of "this.price" from a Pizza object"
-Expect(Pizza.prototype.findPrice().toEqual(10))
+Expect(Pizza.prototype.findPrice(pizzaObject).toEqual(10))
 Test: "it returns a price adjusted by having bacon be a property of a pizza object"
-Expect(Pizza.prototype.findPrice().toEqual(11))
+Expect(Pizza.prototype.findPrice(pizzaObject).toEqual(11))
 Test: "it returns a price adjusted by having Pineapple be a property of a pizza object"
-Expect(Pizza.prototype.findPrice().toEqual(12))
+Expect(Pizza.prototype.findPrice(pizzaObject).toEqual(12))
 Test: "it returns a price adjusted by the size property of a pizza objet, 'expect' based on selecting small"
-Expect(Pizza.prototype.findPrice().toEqual(8))
+Expect(Pizza.prototype.findPrice(pizzaObject).toEqual(8))
+
+function Pizza(){
+  this.meat
+  this.cheese 
+  this.sauce 
+  this.extra 
+  this.size
+  this.price = 10;
+}
+
+pizza = new Pizza;
+
+Pizza.prototype.findPrice = function(pizzaObject){
+  return pizzaObject.price;
+}
